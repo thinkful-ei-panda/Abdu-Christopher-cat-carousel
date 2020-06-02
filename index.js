6d08c48b554556e2c557c95fd57c6dd658e6ce09
Abdu Jemal <abduh9350@gmail.com>
2020-06-02 16:03:27 -0400

    finally completed

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+ function PicturesOnClicks() {
+  $( '.thumbnail' ).click(event => {
+    event.preventDefault();
+    console.log("check1")
+    // src attribute for small image should be applied to large image when clicked
+    const imgSrc = $(event.currentTarget).find('img').attr('src');
+    $('.hero img').attr( 'src', imgSrc);
+    console.log("check2")
+    // alt attribute for small image should be applied to large image when clicked
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
+    $('.hero img').attr( 'alt', imgAlt);
+    console.log("check3")
   });
-  
+  console.log("finalcheck")
+}
+
+$(PicturesOnClicks);
